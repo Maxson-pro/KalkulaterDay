b50063bebb6e3da0d1fbcfd6d06ca0a04af819ae
Maxson-pro <maxmeleshkin2012@gmail.com>
2025-12-24 18:24:30 +0300

    Update CheckDiskTest.java

--- CheckDiskTest.java
+++ CheckDiskTest.java
@@ -2,26 +2,46 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+class ConclusionTest {
 
-class CheckDiskTest {
-
+   @Test
+    void testReturn1_WrongFormat(){
+       assertFalse(Сonclusion.isDate("2025-11"));
+   }
+   @Test
+    void testReturn2_WrongMonth() {
+       assertFalse(Сonclusion.isDate("2025-13-11"));
+   }
+   @Test
+    void testReturn3_WrongDay() {
+       assertFalse(Сonclusion.isDate("2025-04-31"));
+   }
+   @Test
+    void testReturn4_NotNumbers() {
+       assertFalse(Сonclusion.isDate("год-месяц-день"));
+   }
+   @Test
+    void testReturn5_Sussess() {
+       assertTrue(Сonclusion.isDate("2025-11-11"));
+   }
+   @Test
+    void isLeapYear_test1() {
+       assertTrue(Сonclusion.isLeapYear(2024));
+   }
+   @Test
+    void isLeapYear_test2() {
+       assertTrue(Сonclusion.isLeapYear(2000));
+   }
     @Test
-    void getYear() {
-        CheckDisk d = new CheckDisk("2025-11-11");
-        assertEquals(2025, d.getYear());
+    void isLeapYear_test3() {
+        assertFalse(Сonclusion.isLeapYear(2025));
+    }
+    @Test
+    void isLeapYear_test4() {
+        assertFalse(Сonclusion.isLeapYear(1900));
     }
 
-    @Test
-    void getMonth() {
-        CheckDisk d = new CheckDisk("2025-11-11");
-        assertEquals(11, d.getMonth());
-    }
 
-    @Test
-    void getDay() {
-        CheckDisk d = new CheckDisk("2025-11-11");
-        assertEquals(11, d.getDay());
-    }
 }
